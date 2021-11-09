f1b05c3bc36ddccdd2b48f48b560d95ed7ba50a2
bm <cidhighwind85@hotmail.com>
2021-11-09 14:53:10 +0900

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { User } from '.prisma/client';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,5 +10,16 @@ export class AuthService {
 
   async signUp(dto: AuthCredentialsDto): Promise<User> {
     return this.userRepository.create(dto);
+  }
+
+  async signIn(dto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = dto;
+    const user = await this.userRepository.findOne({ username });
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (user && passwordMatches) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Double check your credentials.');
+    }
   }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,11 +1,8 @@
 import { Prisma, User } from '.prisma/client';
-import {
-  BadRequestException,
-  ConflictException,
-  Injectable,
-} from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/db/services/prisma.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserRepository {
@@ -13,9 +10,18 @@ export class UserRepository {
 
   async create(dto: AuthCredentialsDto): Promise<User> {
     try {
-      return await this.prisma.user.create({ data: dto });
+      const { username, password } = dto;
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+      return await this.prisma.user.create({
+        data: { username, password: hashedPassword },
+      });
     } catch {
       throw new ConflictException('Username already exists');
     }
+  }
+
+  async findOne(where: Prisma.UserWhereInput): Promise<User> {
+    return this.prisma.user.findFirst({ where });
   }
 }
